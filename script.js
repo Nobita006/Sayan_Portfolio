21f6061dedc32c0d4a83878933e60bfdbdfce6e4
Sayan Das <sayandas001s@gmail.com>
2024-07-06 16:19:41 +0530

    Modified resume

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxA9BTmqLVzaAFmBBUW8qOkLGez5TcLVJs0-Z7x8hlthtR5fOVobE6ZE4rpLE8xmzJndQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
 const homesection = document.querySelector("#Home");
 const navbar = document.querySelector(".navbar");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      msg.innerHTML = "Message sent succesfully!!"
+      setTimeout(function () {
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+    })
+    .catch(error => {
+    console.error('Error!', error.message)
+    msg.innerHTML = "An error occurred. Please try again later."
+    })
+    
+})
 
 const observer = new IntersectionObserver(
     (entries)=>{
